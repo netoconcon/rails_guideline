94d7e90ef9bbf3af64ac2f49cbba33458a094875
Neto Concon <concon.neto2@gmail.com>
2018-11-03 22:10:03 -0300

    add fontawesome and bootstrap

--- 01_Setup.rb
+++ 01_Setup.rb
@@ -29,3 +29,21 @@ gem 'simple_form'
 >> terminal >> rails db:create # cria o database
 
 
+*********** Fontawesome e Bootstrap *************************
+
+# Adicionar o link do Fontawesome e do Bootstrap no head do:
+
+# /views/layouts/application.html.erb
+
+# bootstrap
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+# fontawesome
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+
+
+
